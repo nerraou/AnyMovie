c979f0515ebc217399b6d36600803a9f41c48599
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-05-05 15:30:25 +0100

    feat: create cast card

---
+++ frontend/src/app/components/molecules/CastCard.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+interface CastCardProps {
+  name: string;
+  image: string;
+  characterName: string;
+}
+
+function CastCard(props: CastCardProps) {
+  return (
+    <div className="flex flex-col bg-white rounded-lg w-28 shadow-lg">
+      <div className="relative w-full h-40">
+        <Image
+          alt="character image"
+          src={props.image}
+          fill
+          className="absolute object-cover rounded-t-lg overflow-hidden"
+        />
+      </div>
+
+      <div className="space-y-2 p-2">
+        <p>{props.characterName}</p>
+        <p>{props.name}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CastCard;
