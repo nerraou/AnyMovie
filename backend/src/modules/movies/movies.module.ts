644b79da19d3eb48fe826859fda10abc6bd0f1de
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-05-07 00:09:04 +0100

    fix: remove unused import

--- backend/src/modules/movies/movies.module.ts
+++ backend/src/modules/movies/movies.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
-import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [CacheModule.register()],
   providers: [MoviesService],
   controllers: [MoviesController],
 })
